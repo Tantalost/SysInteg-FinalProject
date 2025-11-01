73bae6da4117638a14ab9ab625fe6e3b20527a20
Lol <133163245+Tantalost@users.noreply.github.com>
2025-11-01 15:28:47 +0800

    feat: added roomcards in the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import FeaturedRoom from '../components/FeaturedRoom'
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <FeaturedRoom />
     </>
   )
 }
