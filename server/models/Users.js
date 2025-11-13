cdf202ab4142386ebd75266366606cfdad806438
Lol <133163245+Tantalost@users.noreply.github.com>
2025-11-13 17:52:19 +0800

    test:

--- server/models/Users.js
+++ server/models/Users.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     __id: {type: String, required: true},
     username: {type: String, required: true},
     email: {type: String, required: true},
-    image: {type: String, required: true},
+    image: {type: String},
     role: {type: String, enum: ['user', 'admin'], default: 'user'},
     recentSearchRooms: [{type: String}],
 }, {timestamps: true}
