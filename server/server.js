114e3f8a09619cf767903b57d7f8db01b1880dfe
Lol <133163245+Tantalost@users.noreply.github.com>
2025-11-19 00:18:27 +0800

    feat: all routes api

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import userRouter from './routes/userRoutes.js';
 import roomRouter from './routes/roomRoutes.js';
 import connectCloudinary from './configs/cloudinary.js';
 import propertyRouter from './routes/propertyRoutes.js';
+import bookingRouter from './routes/bookingRoutes.js';
 
 
 connectDB()
@@ -27,6 +28,8 @@ app.get('/', (req, res)=> res.send("API is working"))
 app.use('/api/user', userRouter)
 app.use('/api/rooms', roomRouter)
 app.use('/api/properties', propertyRouter)
+app.use('/api/bookings', bookingRouter)
+
 
 const PORT = process.env.PORT || 3000;
 
